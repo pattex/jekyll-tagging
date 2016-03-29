4cfc6830ffe2a4a7f726dd9c3ad269dbb98c5cfa
Todd Eichel <todd@toddeichel.com>
2016-03-29 10:50:54 -0700

    Add ability to append extra data to all tag pages or tag feeds.

--- lib/jekyll/tagging.rb
+++ lib/jekyll/tagging.rb
@@ -48,7 +48,8 @@ module Jekyll
     def new_tag(tag, posts)
       self.class.types.each { |type|
         if layout = site.config["tag_#{type}_layout"]
-          data = { 'layout' => layout, 'posts' => posts.sort.reverse!, 'tag' => tag }
+          data = site.config["tag_#{type}_data"] || {}
+          data.merge!('layout' => layout, 'posts' => posts.sort.reverse!, 'tag' => tag)
 
           name = yield data if block_given?
           name ||= tag
