fb95013097a89434afb25a0681b19cc2c4f66260
Stefan Niederhauser <ghuder5@gmx.ch>
2017-10-20 15:12:52 +0200

    undo other changes

--- lib/jekyll/tagging.rb
+++ lib/jekyll/tagging.rb
@@ -29,19 +29,12 @@ module Jekyll
 
     class << self; attr_accessor :types, :site; end
 
-    @@classifiers={
-        'log'=>proc{|classes,value,min,max|
-          scaled = (classes*(Math.log(value) - Math.log(min))/(Math.log(max) - Math.log(min))).to_i
-          scaled == classes ? classes : scaled+1},
-        'linear'=>proc{|classes,value,min,max| (1..max).quantile(value, classes)}
+    @@classifiers = {
+        'log' => proc{|classes, value, min, max|
+          scaled = (classes * (Math.log(value) - Math.log(min)) / (Math.log(max) - Math.log(min))).to_i
+          scaled == classes ? classes : scaled + 1},
+        'linear' => proc{|classes, value, min, max| (1..max).quantile(value, classes)}
     }
-
-    def initialize(config)
-      @ignored_tags=config["ignored_tags"] || []
-      @threshold=(config["tag_threshold"] || '1').to_i
-      @permalink_style=config['tag_permalink_style']
-      @classifier=config['tag_classifier'] || 'linear'
-    end
 
     def generate(site)
       self.class.site = self.site = site
@@ -82,7 +75,8 @@ module Jekyll
     end
 
     def add_tag_cloud(num = 5, name = 'tag_data')
-      s, t = site, { name => calculate_tag_cloud(@@classifiers[@classifier].curry[num]) }
+      classifier = config['tag_classifier'] || 'linear'
+      s, t = site, { name => calculate_tag_cloud(@@classifiers[classifier].curry[num]) }
       s.respond_to?(:add_payload) ? s.add_payload(t) : s.config.update(t)
     end
 
@@ -93,15 +87,15 @@ module Jekyll
     def calculate_tag_cloud(classifier)
       tags = active_tags.map { |tag, posts| [tag, posts.size] }
       min, max = tags.map { |tag, size| size }.minmax
-      tags.sort!.map! { |tag, size| [tag, classifier.call(size,min,max)] }
+      tags.sort!.map! { |tag, size| [tag, classifier.call(size, min, max)] }
     end
 
     def active_tags
-      site.tags.reject { |tag, posts| @ignored_tags.include? tag or posts.size < @threshold}
+      site.tags.reject { |t| site.config["ignored_tags"].include? t[0]}
     end
 
     def pretty?
-      @pretty ||= (site.permalink_style == :pretty || @permalink_style == 'pretty')
+      @pretty ||= (site.permalink_style == :pretty || site.config['tag_permalink_style'] == 'pretty')
     end
 
   end
@@ -155,7 +149,8 @@ module Jekyll
     end
 
     def active_tag_data(site = Tagger.site)
-      return site.config['tag_data']
+      return site.config['tag_data'] unless site.config["ignored_tags"]
+      site.config["tag_data"].reject { |tag, set| site.config["ignored_tags"].include? tag }
     end
   end
 
